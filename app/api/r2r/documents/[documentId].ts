811916346348084f56c9eeb1ddb1a2b11fb3d073
Buns Enchantress <68980965+BunsDev@users.noreply.github.com>
2025-03-01 22:08:01 -0600

    Update [documentId].ts

--- app/api/r2r/documents/[documentId].ts
+++ app/api/r2r/documents/[documentId].ts
@@ -2,7 +2,7 @@
 // import { NextResponse } from 'next/server';
 
 
-export async function DELETE() {
+export async function DELETE(request) {
   console.log({ request })
   try {
     const filters = {
