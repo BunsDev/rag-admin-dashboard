dd56c0fe7b555b397a16d3f5cb3cf64ceba611f8
Buns Enchantress <68980965+BunsDev@users.noreply.github.com>
2025-03-01 22:09:28 -0600

    Update input.tsx

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -3,8 +3,8 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 
 export interface InputProps {
-  className?: any;
-  type?: any;
+  className?: string;
+  type?: string;
 }
  // extends React.InputHTMLAttributes<HTMLInputElement> {}
 
